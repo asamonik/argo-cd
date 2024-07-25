93dfbd85a11247d5cac88bdb065c7ced40b83f89
asamonik <49288593+asamonik@users.noreply.github.com>
2024-07-25 09:46:00 +0200

    KeptnMetric empty error
    Signed-off-by: asamonik <49288593+asamonik@users.noreply.github.com>

--- resource_customizations/metrics.keptn.sh/KeptnMetric/health.lua
+++ resource_customizations/metrics.keptn.sh/KeptnMetric/health.lua
@@ -1,10 +1,10 @@
 local hs = {}
-if obj.status.errMsg == nil and obj.status.value ~= nil then
+if (obj.status.errMsg == nil or obj.status.errMsg == "") and obj.status.value ~= nil then
     hs.status = "Healthy"
     hs.message = "KeptnMetric is healthy"
     return hs
 end
-if obj.status.errMsg ~= nil then
+if obj.status.errMsg ~= nil and obj.status.errMsg ~= "" then
     hs.status = "Degraded"
     hs.message = "KeptnMetric is degraded"
     return hs
